5b12164fd222c2c65a9548d9ce4a9bb9efbc9a99
CamPartridge <cpartridge@student.neumont.edu>
2024-05-03 09:11:41 -0600

    UserService Dockerized

--- backend/userService/dbconfig/config.js
+++ backend/userService/dbconfig/config.js
@@ -1,23 +1,13 @@
 const Sequelize = require("sequelize")
 
-// const sequelize = new Sequelize(
-//     'UserDB',
-//     'root',
-//     'slay123!',
-//     {
-//         host: 'mysql_userDB',
-//         dialect: 'mysql'
-//     }
-// );
-
 const sequelize = new Sequelize(
-  'userdb',
-  'root',
-  'D&D4bookwyrmbam',
-   {
-     host: 'localhost',
-     dialect: 'mysql'
-   }
- );
+    'UserDB',
+    'root',
+    'slay123!',
+    {
+        host: 'mysql_userDB',
+        dialect: 'mysql'
+    }
+);
 
 module.exports = sequelize
