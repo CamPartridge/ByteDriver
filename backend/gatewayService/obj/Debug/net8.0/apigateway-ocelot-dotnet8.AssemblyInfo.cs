4aa9768586fbd57f0fa82f0318f5aacbcaa706fb
eaheil <129311664+eaheil@users.noreply.github.com>
2024-05-21 09:48:45 -0600

    object updates

--- backend/gatewayService/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
+++ backend/gatewayService/obj/Debug/net8.0/apigateway-ocelot-dotnet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed32ba554824dc45bb27d985fe25c8618e2ae0e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33adfeb8ea3a74f59d39c7e5abb2b2c13ae1079")]
 [assembly: System.Reflection.AssemblyProductAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("apigateway-ocelot-dotnet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
